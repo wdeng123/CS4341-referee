23cb9ab3abc8f55edcfbccf57ec74ff03788062d
Jacob Molnia <jrmolnia@wpi.edu>
2025-01-05 01:05:11 +0100

    init

---
+++ tests/test_referee.py
@@ -0,0 +1,17 @@
+"""Package description goes here.
+
+You can add more detailed information about what this package does,
+what modules it contains, and how to use it.
+"""
+
+
+def test_basic() -> None:
+    """Basic test to ensure testing works."""
+    assert True
+
+
+def test_import() -> None:
+    """Test that we can import our package."""
+    import src
+
+    assert src is not None
